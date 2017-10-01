f0383637c8b3a66f0276fb9a64c2b82a01549ea9
Chris Nunez <cenunez47@gmail.com>
2017-10-01 21:54:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function shout(x) {
 function whisper(x) {
   return x.toLowerCase()
 }
+
+function sayHiToGrandma(x) {
+  if x.toLowerCase()
+  return "I can't hear you!"
+  else if x.toUpperCase()
+  return "YES INDEED!"
+}
